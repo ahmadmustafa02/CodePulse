ad885b4943ba8ddb1a6ec86c66e99c6aa458e238
Ahmad Mustafa <ahmadmustafabutt.dev@gmail.com>
2026-05-26 10:16:17 +0500

    reverted back to before hackathon

---
+++ web/android/app/src/main/assets/public/assets/index-Bdund-J1.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-C2PZoeMB.js","assets/index-BW6nL4pQ.js"])))=>i.map(i=>d[i]);
+import{Q as e,_ as o}from"./index-BW6nL4pQ.js";const _=e("Browser",{web:()=>o(()=>import("./web-C2PZoeMB.js"),__vite__mapDeps([0,1])).then(r=>new r.BrowserWeb)});export{_ as Browser};
